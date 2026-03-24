2fe204b9b61b5b32dc573e51445bcefc03917756
maxon390 <maxon390@gmail.com>
2026-03-24 15:42:15 +0200

    added a bot body

--- main.py
+++ main.py
@@ -9,13 +9,14 @@ load_dotenv()
 TOKEN_BOT = os.getenv("TOKEN_TELEGRAM")
 
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(update.effective_message.id,"Здарова")
+    await context.bot.send_message(update.effective_message.chat_id,"Здарова")
 
 
 
 app = ApplicationBuilder().token(TOKEN_BOT).build()
 
 app.add_handler(CommandHandler("hello",hello))
-
+print("Бот працює...")
 app.run_polling()
+
 
